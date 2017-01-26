90b11176d9bcc4898ddbc4c12886518716e3b06e
Christopher Chan <christopherchan35@gmail.com>
2017-01-26 11:53:07 -0800

    add the Greetr function to global window and give it an alias of G$

--- section_9/Greetr.js
+++ section_9/Greetr.js
@@ -4,6 +4,7 @@
     }
   }
 
+  // inside prototype is where we put methods that we will want to use in the object that is returned from Greetr
   Greetr.prototype = {};
 
   // use self so we don't need to worry about what the current value of 'this' is
@@ -13,4 +14,10 @@
     self.lastName = lastName || '';
     self.language = language || 'en';
   }
+  // objects created by Greetr.init have their prototype pointed at Greetr.init.prototype. Here we make it point to Greetr.prototype created above
+  Greetr.init.prototype = Greetr.prototype;
+
+  // add Greetr to global object so that it can be called anywhere like jQuery or $
+  global.Greetr = global.G$ = Greetr;
+
 }(window, jQuery));
